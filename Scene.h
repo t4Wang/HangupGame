f32b4bfa76bc82f1aff612b7c7058ca2e47407f9
wrzhxy <wrzhxy@qq.com>
2017-07-19 22:34:03 +0800

    Scene.h add members

--- Scene.h
+++ Scene.h
@@ -3,11 +3,16 @@
  * 场景类：记录当前场景信息
  */
 class Scene {
-	// genera
-	// 生成事件
-	void generateEvent();
+public:
 	// 打印场景信息
 	void printBrief();
-
-	// level
+	// 生成事件 使用当前地形信息，生成对应的事件
+	void generateEvent();
+private:
+	int level;
+	string brief;
+	string name;
+	string scene_id;
+	set<Monster> monsters;
+	// set<NPC> npcs;
 }
