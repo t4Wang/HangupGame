6cbb7fe518d77dee1758a85239ae32b9147131d8
wrzhxy <wrzhxy@qq.com>
2017-07-20 08:42:08 +0800

    create Character and Monster

--- Scene.h
+++ Scene.h
@@ -4,15 +4,18 @@
  */
 class Scene {
 public:
-	// 打印场景信息
-	void printBrief();
+	// 场景初始化
+	void init();
 	// 生成事件 使用当前地形信息，生成对应的事件
 	void generateEvent();
 private:
+	// 打印场景信息
+	void printBrief();
+	
+	string scene_id;
 	int level;
 	string brief;
 	string name;
-	string scene_id;
-	set<Monster> monsters;
+	// set<Monster> monsters;
 	// set<NPC> npcs;
 }
